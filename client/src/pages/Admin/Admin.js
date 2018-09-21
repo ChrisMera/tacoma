b480086fd96e427e30c9155c755a1f87fefd7a5a
Chupi <cmera001@fiu.edu>
2018-09-21 11:13:11 -0400

    Updated admin page to take in container, col, row components

--- client/src/pages/Admin/Admin.js
+++ client/src/pages/Admin/Admin.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Menu, MenuItem } from "../../components/Menu";
 import "./Admin.css";
-// import { Col, Row, Container } from "../../components/Grid";
+import { Col, Row, Container } from "../../components/Grid";
 
 class Admin extends Component {
   state = {
@@ -10,20 +10,21 @@ class Admin extends Component {
 
   render() {
     return (
-      
-        <div className="container">
-          <div className="row">
-            <div className="col-md-2 menu">
+      <Container fluid>
+        <Row className="info-dash">
+          <Col size="md-2" className="menu">
             <Menu>
               <MenuItem></MenuItem>
             </Menu>
-            </div>
-            <div className="col-md-8 info-dash">
-            Info Dash
-            </div>
-          </div>
-        </div>
-     
+          </Col>
+          <Col size="md-8">
+          test
+          </Col>
+          <Col size="md-2">
+          api
+          </Col>
+        </Row>
+      </Container>
     )
   }
 
