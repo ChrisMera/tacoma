16e042e1e2caadaabdbd52aae2e50d8dadc2e259
miramira <cmera001@fiu.edu>
2018-10-30 10:34:23 -0400

    Navbar is responsive at widths between 400px to 2500px/

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -12,33 +12,25 @@ class Nav extends Component {
   render() {
     const isLoggedIn = this.state.isLoggedIn;
     return(
-    <nav className="navbar sticky-top navbar-expand-lg" id="custom-navbar">
-      <p className="navbar-brand">Tacoma Transport</p>
-          { !isLoggedIn ? (
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <button type="button" className="quote-btn" data-toggle="modal" data-target="#requestQuote">
-                    Request Quote
-                  </button>
-                </li>
-                <li className="nav-item">
-                    <Login />
-                </li>
-              </ul>
-            </div>
-          ) : (
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                </li>
-                <li className="nav-item">
-                  Logout
-                </li>
-              </ul>
-            </div>
-          )}
-    </nav>
+
+      <nav class="navbar sticky-top navbar-expand-lg navbar-dark">
+        <a class="navbar-brand" href="/">Tacoma Transport</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <button type="button" className="quote-btn" data-toggle="modal" data-target="#requestQuote">
+                Request Quote
+              </button>
+            </li>
+            <li class="nav-item">
+              <Login />
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
